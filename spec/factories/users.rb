e82c30cf12b27f4728230457f8a22bd68c8d3bfd
Timur Karimov <mathcore_detected@mail.ru>
2022-12-03 14:48:35 +0500

    upd users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :user, class: User do
     email { Faker::Internet.email }
     password { Faker::Internet.password(min_length: 8) }
+    gender { [:male, :female].sample }
   end
 end
